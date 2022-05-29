d50cdb633954adf1d5f1a3cafeecc6a8b76bf150
mkiszka <32388178+mkiszka@users.noreply.github.com>
2022-05-29 09:08:44 +0200

    Empty div when no current song.

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -3,14 +3,19 @@ import { Heading } from "./Heading";
 import "./SongPlayer.css";
 
 export function SongPlayer({ audioRef, showControls = false, song }) {
-  const { audioUrl, coverUrl } = song == null ? ["none", "none"] : song;
+  const { audioUrl, coverUrl } = song == null ? [null, null] : song;
   useEffect(() => {
     console.log("a");
   });
   return (
     <section className="SongPlayer">
       <Heading title="Music Player" />
-      <img width="250" height="250" src={coverUrl} alt="Song cover" />
+      {coverUrl == null ? (
+        <div style={{ height: "250px", width: "250px", clear: "both" }}></div>
+      ) : (
+        <img width="250" height="250" src={coverUrl} alt="Song cover" />
+      )}
+
       <audio ref={audioRef} key={audioUrl} controls={showControls}>
         {" "}
         pl
